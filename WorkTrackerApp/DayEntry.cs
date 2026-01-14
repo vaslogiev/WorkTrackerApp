1cde98bb7a3991d2e302ad76ba55a6b932aead48
Vasil Evlogiev <adesso.vasil.evlogiev@vwfspay.com>
2026-01-14 15:20:30 +0200

    Add project files.

---
+++ WorkTrackerApp/DayEntry.cs
@@ -0,0 +1,24 @@
+﻿using Postgrest.Attributes;
+using Postgrest.Models;
+
+public enum DayStatus { None, Worked, Paid, Finished }
+
+[Table("work_days")]
+public class DayEntry : BaseModel
+{
+  [PrimaryKey("id", false)]
+  public string Id { get; set; }
+
+  [Column("date")]
+  public DateTime Date { get; set; }
+
+  [Column("status")]
+  public int StatusInt { get; set; }
+
+  // Помощно свойство за работа с Enum
+  public DayStatus Status
+  {
+    get => (DayStatus)StatusInt;
+    set => StatusInt = (int)value;
+  }
+}
---
+++ WorkTrackerApp/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Supabase;
+using WorkTrackerApp;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped(provider =>
+    new Client(
+        "https://rxvuylvhbsoepnclbaiw.supabase.co",
+        "sb_publishable_XssO8nOqlteCn7eUpjOp1g_LaGRnrZH",
+        new SupabaseOptions { AutoConnectRealtime = true }
+    )
+);
+
+await builder.Build().RunAsync();
