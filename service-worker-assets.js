b1558ea70bab258b4dd6f6cad7830cb52d274250
vaslogiev <mcg.vasko@gmail.com>
2026-01-14 20:04:29 +0000

    Deploying to gh-pages from @ vaslogiev/WorkTrackerApp@9b23c9de6de962a1fda0dbbd5424fc2c017fd7b5 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -1,5 +1,5 @@
 self.assetsManifest = {
-  "version": "H2GMw9hu",
+  "version": "zqi6aaPy",
   "assets": [
     {
       "hash": "sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=",
@@ -354,11 +354,11 @@ self.assetsManifest = {
       "url": "_framework/Websocket.Client.wasm"
     },
     {
-      "hash": "sha256-eo7z3M1NkZbGw/E4E74VY7VBo2b0a+RcLgBXSMcqNbs=",
+      "hash": "sha256-H4Lf4WaE5cpibjf21k+DYMk95JOnZFC/mbBXq6SmXCc=",
       "url": "_framework/WorkTrackerApp.wasm"
     },
     {
-      "hash": "sha256-mprLqJkF4QUpL+OLxNYmZ2utd5nrf/yiFVw7CQ4oU/E=",
+      "hash": "sha256-BNHLa2pQVcvXWA4kfJxKhw2bcJwZaPMLad8Rpw0pxp4=",
       "url": "_framework/blazor.boot.json"
     },
     {
